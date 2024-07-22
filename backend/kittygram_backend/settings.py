5a17fa05755ddc04670d585f7ae66e4b753f3e8c
Khamtsev <dhamcev@yandex.ru>
2024-07-22 13:28:35 +0300

    Test push

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', default='token')
+SECRET_KEY = os.getenv('SECRET_KEY', default='secret_token')
 
 DEBUG = os.getenv('DEBUG', default='false').lower() in ('true', '1')
 
