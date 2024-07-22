b8c038e5ba8aa93f0738278c7b902e69ce50aef1
Khamtsev <dhamcev@yandex.ru>
2024-07-22 13:18:01 +0300

    Add default allowed_hosts

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='token')
 
 DEBUG = os.getenv('DEBUG', default='false').lower() in ('true', '1')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
